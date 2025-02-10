8e65f08f6e1696e860a17bb9796fc6f22e909b5a
Шереметьева <aseremetevo@gmail.com>
2025-02-10 21:15:10 +0200

    add two argument

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,11 +4,14 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-.name('gendiff')
-.description('Compares two configuration files and shows a difference.')
-.version('1.0.0')
-.action((options) => {
-  console.log(choise(options.format));
-});
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(`filepath1: ${filepath1}, filepath2: ${filepath2}`);
+  });
 
 program.parse();
